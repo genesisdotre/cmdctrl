cb9123b7c9a205e0016964dd05587da7314b06fd
Michal Stefanow <mstefanow@gmail.com>
2013-09-08 16:28:36 +0100

    cleanup

--- config.js
+++ config.js
@@ -1,13 +1,18 @@
 
 	
-	var blockOfHTML = 
-	'<h2>#indiewebcamp YO YO YO</h2><br>' + 
-	'<img src="http://michalstefanow.com/img/2008.jpg"/>' +
-	'<img src="http://michalstefanow.com/img/2009.jpg"/>' +
-	'<img src="http://michalstefanow.com/img/2010.jpg"/>' +
-	'<img src="http://michalstefanow.com/img/2011.jpg"/>' +
-	'<img src="http://michalstefanow.com/img/2012.jpg"/>' +
-	'<img src="http://michalstefanow.com/img/2013.jpg"/>' +
-	'<img src="http://img513.imageshack.us/img513/4706/s86.gif"/>';
+	
+var blockOfHTML = 
+'<h2>Hello World!</h2><br>' + 
+'<img src="http://michalstefanow.com/img/2008.jpg" width="100"/>' +
+'<img src="http://michalstefanow.com/img/2009.jpg" width="100"/>' +
+'<img src="http://michalstefanow.com/img/2010.jpg" width="100"/>' +
+'<img src="http://michalstefanow.com/img/2011.jpg" width="100"/>' +
+'<img src="http://michalstefanow.com/img/2012.jpg" width="100"/>' +
+'<img src="http://michalstefanow.com/img/2013.jpg" width="100"/>' +
+'<img src="http://img513.imageshack.us/img513/4706/s86.gif" width="100"/>';
 
-	var openOnStart = false;
+var openOnStart = false;
+
+var defaultText = "Top Secret";
+
+var dragText = "Open source FTW";
