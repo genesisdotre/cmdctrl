4a91ffb567901e2a07725db9250a450a0c91c597
Michal Stefanow <mstefanow@gmail.com>
2014-09-28 22:01:08 +0100

    fix for duplicate Mailchimp signup forms

--- cmdctrl.js
+++ cmdctrl.js
@@ -28,7 +28,7 @@
 
 			"	<div id='mc_embed_signup'>" +
 			"	<form action='" + endpoint + "' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' class='validate' target='_blank' novalidate>" +
-			"		<input type='email' value='' name='EMAIL' class='email' id='mce-EMAIL' placeholder='email address' required>" +
+			"		<input type='email' value='' name='EMAIL' class='email' id='mce-EMAIL-XXX' placeholder='email address' required>" +
 			"		<div style='position: absolute; left: -5000px;'><input type='text' name='b_f7d4defd21860779ebd2e03af_e6e9f2a096' tabindex='-1' value=''></div>" +
 			"		<div class='clear'><input type='submit' value='Count me in' name='subscribe' id='mc-embedded-subscribe' class='button'></div>" +
 			"	</form>" +
@@ -58,7 +58,7 @@
 		$("#closebutton, #closetext").on("click", _closePopup);
 
     $("#mc-embedded-subscribe-form").on("submit", function() {
-      var email = $("#mce-EMAIL").val();
+      var email = $("#mce-EMAIL-XXX").val();
 
       $.post(endpoint, { email: email }, function(response){
         console.log("got response: " + response);
