31e7554404df9f6bb79d94717590b9bf5b404ad4
Michal Stefanow <mstefanow@gmail.com>
2015-06-29 23:58:23 +0100

    everything is loaded optionally #ok

--- cmdctrl.js
+++ cmdctrl.js
@@ -1,17 +1,19 @@
-window.addEventListener("message", function(event) {
-  if(event.data === "remove_iframe") {
-    var iframe = document.getElementById("cmdctrl");
-    iframe.parentNode.removeChild(iframe);
-  }
-});
-
-var iframe = document.createElement("iframe");
-iframe.id = "cmdctrl";
-iframe.src = "http://genesis.re/cmdctrl/cmdctrl-iframe.html";
-iframe.style.position = "fixed";
-iframe.style.top = 0;
-iframe.style.left = 0;
-iframe.style.height = "100%";
-iframe.style.width = "100%";
-iframe.style["z-index"] = 9999999;
-document.body.appendChild(iframe);
+if (window.location.hash.indexOf("ok") !== -1) {
+  window.addEventListener("message", function(event) {
+    if(event.data === "remove_iframe") {
+      var iframe = document.getElementById("cmdctrl");
+      iframe.parentNode.removeChild(iframe);
+    }
+  });
+  
+  var iframe = document.createElement("iframe");
+  iframe.id = "cmdctrl";
+  iframe.src = "http://genesis.re/cmdctrl/cmdctrl-iframe.html";
+  iframe.style.position = "fixed";
+  iframe.style.top = 0;
+  iframe.style.left = 0;
+  iframe.style.height = "100%";
+  iframe.style.width = "100%";
+  iframe.style["z-index"] = 9999999;
+  document.body.appendChild(iframe);
+}
