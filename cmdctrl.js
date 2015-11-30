bae85799a0b68371ecc8f7f45b92309389db0805
Michal Stefanow <mstefanow@gmail.com>
2015-11-30 17:13:24 +0100

    It is kind of cool now!

--- cmdctrl.js
+++ cmdctrl.js
@@ -1,4 +1,15 @@
-if (window.location.hash.indexOf("ok") !== -1) {
+var displayCondition = function() {
+  var notOK = ["genesis.re/cmdctrl", "mostlydoing.com"];
+  for (var i=0; i<notOK.length; i++) {
+    if (document.referrer.indexOf(notOK[i]) !== -1) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+if (displayCondition()) {
   window.addEventListener("message", function(event) {
     if(event.data === "remove_iframe") {
       var iframe = document.getElementById("cmdctrl");
@@ -8,7 +19,8 @@ if (window.location.hash.indexOf("ok") !== -1) {
   
   var iframe = document.createElement("iframe");
   iframe.id = "cmdctrl";
-  iframe.src = "http://genesis.re/cmdctrl/cmdctrl-iframe.html";
+  // iframe.src = "http://genesis.re/cmdctrl/cmdctrl-iframe.html";
+  iframe.src = "cmdctrl-iframe.html";
   iframe.style.position = "fixed";
   iframe.style.top = 0;
   iframe.style.left = 0;
@@ -17,3 +29,8 @@ if (window.location.hash.indexOf("ok") !== -1) {
   iframe.style["z-index"] = 9999999;
   document.body.appendChild(iframe);
 }
+
+
+var style = document.createElement("style");
+style.innerText = "h1 { color : red }";
+document.body.appendChild(style);
