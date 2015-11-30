c96d0e413341d13ccafb491f549a3202bb7ce56f
Michal Stefanow <mstefanow@gmail.com>
2015-11-30 18:02:21 +0100

    GETTING THERE!

--- cmdctrl.js
+++ cmdctrl.js
@@ -1,42 +1,3 @@
-// var displayCondition = function() {
-//   var notOK = ["genesis.re/cmdctrl", "mostlydoing.com"];
-//   for (var i=0; i<notOK.length; i++) {
-//     if (document.referrer.indexOf(notOK[i]) !== -1) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// };
-
-// if (displayCondition()) {
-//   window.addEventListener("message", function(event) {
-//     if(event.data === "remove_iframe") {
-//       var iframe = document.getElementById("cmdctrl");
-//       iframe.parentNode.removeChild(iframe);
-//     }
-//   });
-  
-//   var iframe = document.createElement("iframe");
-//   iframe.id = "cmdctrl";
-//   // iframe.src = "http://genesis.re/cmdctrl/cmdctrl-iframe.html";
-//   iframe.src = "cmdctrl-iframe.html";
-//   iframe.style.position = "fixed";
-//   iframe.style.top = 0;
-//   iframe.style.left = 0;
-//   iframe.style.height = "100%";
-//   iframe.style.width = "100%";
-//   iframe.style["z-index"] = 9999999;
-//   document.body.appendChild(iframe);
-// }
-
-
-// var style = document.createElement("style");
-// style.innerText = "h1 { color : red }";
-// document.body.appendChild(style);
-
-//$("body").append("<h1>helloworld</h1>");
-
 var markup =  "<div id='cmdctrl'>" +
               "  <canvas id='cmdctrl-animation'></canvas>" +
               "  <div id='cmdctrl-fade'></div>" +
@@ -66,8 +27,8 @@ $(function() {
     $("#cmdctrl-fade").addClass("hidden");
     $("#cmdctrl-animation").addClass("hidden");
     setTimeout(function() { 
-      parent.postMessage("remove_iframe", "*");
-    }, 1000);
+      $("#cmdctrl-closebutton, #cmdctrl-closetext, #cmdctrl-fade").remove();
+    }, 600);
     return false;
   };
 
